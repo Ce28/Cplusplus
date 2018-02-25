28cf103f5923e366be0f06fa3764773378637bab
Ce28 <christian.sapien@hotmail.com>
2018-02-25 13:19:51 -0700

    Errores sintácticos y lógicos

---
+++ 04/Act1.SuperficieCuadrado/Act1.SuperficieCuadrado/archivo1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	int iLado = 0;
+	int iSuperficie;
+	cout << "Ingrese el valor del lado del cuadrado: ";
+	cin >> iLado;
+	iSuperficie = iLado * iLado;
+	cout << "La superficie del cuadradro es: ";
+	cout << iSuperficie;
+
+	cin.get();
+	cin.get();
+}
